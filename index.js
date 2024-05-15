1078745899fa4f707c5cd2697180b1ac1163377b
Vincent Tse <river_run@126.com>
2024-05-15 17:41:41 +0800

    1.0.3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-export const hello = 'greetings from mvc'
+import { metaContractIcons } from './rules/metaContract'
+
+export function getIconUri(params) {
+  if (params.type === 'metaContract') {
+    const logoName = metaContractIcons.find(
+      (icon) => icon.genesis === params.genesis && icon.codehash === params.codehash,
+    )?.logo
+
+    if (!logoName) {
+      return ''
+    }
+
+    return `https://unpkg.com/@mvc-org/mvc-resources/icons/meta-contract/${params.icon}`
+  }
+
+  return `https://unpkg.com/@mvc-org/mvc-resources/icons${params.icon}`
+}
